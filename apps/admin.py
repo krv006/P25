064c68b41e348167b8ecb6d23f35aa6d53180e07
krv006 <rvkamronbek@gmail.com>
2024-09-19 14:47:07 +0500

    add

--- apps/admin.py
+++ apps/admin.py
@@ -1,26 +1,11 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from apps.models import Account, Friend, Product, Category
+from apps.models import Product
 
 
 # admin.site.register(Account)
 
-@admin.register(Account)
-class AccountAdmin(ModelAdmin):
-    pass
-
-
 @admin.register(Product)
 class ProductAdmin(ModelAdmin):
-    pass
-
-
-@admin.register(Category)
-class CategoryAdmin(ModelAdmin):
-    pass
-
-
-@admin.register(Friend)
-class FriendAdmin(ModelAdmin):
     pass
--- apps/urls.py
+++ apps/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from apps.views import profil_page, product_page, index
+from apps.views import index
 
 urlpatterns = [
-    path('profil/', profil_page, name='profil_page'),
-    path('product/page/<int:pk>', product_page, name='product_page'),
-    path('category/', index, name='index-page')
+    # path('profil/', profil_page, name='profil_page'),
+    # path('product/page/<int:pk>', product_page, name='product_page'),
+    path('', index, name='index-page')
 ]
