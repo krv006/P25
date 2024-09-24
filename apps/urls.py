710646e71cb13adec8bb4c1130a424bdd0d19f1f
krv006 <rvkamronbek@gmail.com>
2024-09-24 18:25:35 +0500

    add

--- apps/urls.py
+++ apps/urls.py
@@ -1,9 +1,4 @@
 from django.urls import path
 
-from apps.views import index
-
 urlpatterns = [
-    # path('profil/', profil_page, name='profil_page'),
-    # path('product/page/<int:pk>', product_page, name='product_page'),
-    path('', index, name='index-page')
 ]
